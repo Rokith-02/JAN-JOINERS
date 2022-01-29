7d29a3c4e515bcf4a0833e447e34ba409d8e3ce1
Rokith-02 <98573941+Rokith-02@users.noreply.github.com>
2022-01-29 21:24:50 +0530

    second commit string in recursion

--- they.c
+++ they.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+void fun(char*a)
+{
+  if(*a)
+  {
+    fun(a+1);
+    printf("%c",*a);
+  }
+}
+int main()
+{
+  char s[20];
+  printf("enter the string\n");
+  scanf("%s",s);
+  fun(s);
+}
